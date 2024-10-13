52985fa3aad7e4d9a0401ce191846d4a294f3c06
nichoth <nichoth@nichoth.com>
2024-10-13 16:41:17 -0700

    expose `encryptBytes`

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { Keychain, plaintextSize, encryptedSize } from './keychain.js'
 export { transformStream } from './transform-stream.js'
+export { encryptBytes } from '@bicycle-codes/simple-aes'
